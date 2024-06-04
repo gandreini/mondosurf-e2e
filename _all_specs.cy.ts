51ae07c1b3e14999588b7f7b04886112531a7cdc
Giulio Andreini <g.andreini@gmail.com>
2024-06-04 06:22:33 +0200

    Test commit.

--- _all_specs.cy.ts
+++ _all_specs.cy.ts
@@ -1,7 +1,7 @@
 import './auth.cy';
 import './basic-navigation.cy';
 import './favorites.cy';
-// import './home.cy';
 // import './pro.cy';
+// import './home.cy';
 // import './calendar.cy';
 // import './map.cy';
