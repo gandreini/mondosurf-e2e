43f2da0cc108ae8e73e4364ac88633de343db9bf
Giulio Andreini <g.andreini@gmail.com>
2024-11-25 18:45:26 +0100

    Pro tests.

--- _all_specs.cy.ts
+++ _all_specs.cy.ts
@@ -2,7 +2,7 @@ import './auth.cy';
 import './basic-navigation.cy';
 import './favorites.cy';
 import './comments.cy';
-// import './pro.cy';
+import './pro.cy';
 // import './home.cy';
 // import './calendar.cy';
 // import './map.cy';
--- pro.cy.ts
+++ pro.cy.ts
@@ -7,7 +7,7 @@ describe('Pro', () => {
     // NEW USER
 
     // New user registration from header
-    it('new user registration from header', () => {
+    /* it('new user registration from header', () => {
         // Registration CTA is there
         cy.getBySel("home-pro-cta-register").should("exist");
 
@@ -19,10 +19,10 @@ describe('Pro', () => {
 
         // Subscription confirmation page displayed.        
         cy.getBySel("subscription-confirmed").should("exist");
-    });
+    }); */
 
     // New user clicks on home CTA
-    it('new user clicks on home CTA', () => {
+    /* it('new user clicks on home CTA', () => {
         // Registration CTA is displayed.
         cy.getBySel("home-pro-cta-register").should("exist");
 
@@ -34,10 +34,10 @@ describe('Pro', () => {
 
         // Subscription confirmation page displayed.
         cy.getBySel("subscription-confirmed").should("exist");
-    });
+    }); */
 
     // New user clicks on home CTA already-member link
-    it('new user clicks on home CTA already a member', () => {
+    /* it('new user clicks on home CTA already a member', () => {
         // Registration CTA is displayed.
         cy.getBySel("home-pro-cta-register").should("exist");
 
@@ -49,7 +49,7 @@ describe('Pro', () => {
 
         // Subscription confirmation page displayed.
         cy.getBySel("subscription-confirmed").should("exist");
-    });
+    }); */
 
     // New user clicks on favorite icon
     // See favorites.cy.ts
@@ -59,23 +59,140 @@ describe('Pro', () => {
 
     // FREE USER
 
-    // Free user login from header
-    it('free user login from header', () => {
-        // Registration CTA is there
-        cy.getBySel("home-pro-cta-register").should("exist");
-
+    // Show pro modal from profile page
+    it('show pro modal from profile page', () => {
         // Click on user in the tab bar to login.
         cy.getBySel('login-tab-bar').click();
 
         // Free user logins.
         cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
 
-        // Trial CTA is there.
-        cy.getBySel("home-pro-cta-trial").should("exist");
+        // Navigate to profile page.
+        cy.getBySel('user-tab-bar').click();
+
+        // Click on Pro button.
+        cy.getBySel('pro-button').click();
+
+        // Check the pro modal.
+        cy.getBySel("pro-modal-content").should("exist");
+        cy.getBySel("pro-modal-web-yearly-button").should("exist");
+        cy.getBySel("pro-modal-web-monthly-button").should("exist");
+    });
+
+    // Show pro modal from profile plan page
+    it('show pro modal from profile plan page', () => {
+        // Click on user in the tab bar to login.
+        cy.getBySel('login-tab-bar').click();
+
+        // Free user logins.
+        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
+
+        // Navigate to profile page.
+        cy.getBySel('user-tab-bar').click();
+
+        // Navigate to plan.
+        cy.getBySel('profile-logged-your-plan').click();
+
+        // Check the pro modal.
+        cy.getBySel("pro-modal-content").should("exist");
+        cy.getBySel("pro-modal-web-yearly-button").should("exist");
+        cy.getBySel("pro-modal-web-monthly-button").should("exist");
+    });
+
+    // Show pro modal from favorite icon
+    it('show pro modal from favorite icon', () => {
+        // Click on user in the tab bar to login.
+        cy.getBySel('login-tab-bar').click();
+
+        // Free user logins.
+        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
+
+        // Go to lillatro page.
+        cy.searchAndGoToSpotPage('lillat');
+
+        // Click on favorite icon.
+        cy.getBySel('favorite-add-button').click();
+
+        // Check the pro modal.
+        cy.getBySel("pro-modal-content").should("exist");
+        cy.getBySel("pro-modal-web-yearly-button").should("exist");
+        cy.getBySel("pro-modal-web-monthly-button").should("exist");
+    });
+
+    // Show pro modal from calendar icon
+    it('show pro modal from calendar icon', () => {
+        // Click on user in the tab bar to login.
+        cy.getBySel('login-tab-bar').click();
+
+        // Free user logins.
+        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
+
+        // Go to lillatro page.
+        cy.searchAndGoToSpotPage('lillat');
+
+        // Click on calendar icon.
+        cy.getBySel('calendar-add-button').click();
+
+        // Check the pro modal.
+        cy.getBySel("pro-modal-content").should("exist");
+        cy.getBySel("pro-modal-web-yearly-button").should("exist");
+        cy.getBySel("pro-modal-web-monthly-button").should("exist");
+    });
+
+    // Show pro modal from full forecast
+    it('show pro modal from full forecast', () => {
+        // Click on user in the tab bar to login.
+        cy.getBySel('login-tab-bar').click();
+
+        // Free user logins.
+        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
+
+        // Go to lillatro page.
+        cy.searchAndGoToSpotPage('lillat');
+        cy.getBySel('surf-spot-tab-forecast').click();
+
+        // Click on full forecast banner.
+        cy.getBySel('surf-spot-full-forecast-banner').click();
+
+        // Check the pro modal.
+        cy.getBySel("pro-modal-content").should("exist");
+        cy.getBySel("pro-modal-web-yearly-button").should("exist");
+        cy.getBySel("pro-modal-web-monthly-button").should("exist");
+    });
+
+    // Test pro outcome pages
+    it.only('test pro outcome pages', () => {
+        // Click on user in the tab bar to login.
+        cy.getBySel('login-tab-bar').click();
+
+        // Free user logins.
+        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
+
+        // Navigate to the monthly subscription success page.
+        cy.visit('/monthly-subscription-success');
+
+        cy.getBySel("subscription-success-empty-list").should("exist");
+        cy.getBySel("subscription-success-title").should("exist");
+
+        // Navigate to the yearly subscription success page.
+        cy.visit('/yearly-subscription-success');
+
+        cy.getBySel("subscription-success-empty-list").should("exist");
+        cy.getBySel("subscription-success-title").should("exist");
+
+        // Navigate to the monthly subscription failed page.
+        cy.visit('/monthly-subscription-cancel');
+
+        cy.getBySel("subscription-failed-title").should("exist");
+
+        // Navigate to the yearly subscription failed page.
+        cy.visit('/yearly-subscription-cancel');
+
+        cy.getBySel("subscription-failed-title").should("exist");
     });
 
     // Free user clicks on home CTA
-    it('free user clicks on home CTA', () => {
+    /* it('free user clicks on home CTA', () => {
         // Registration CTA is displayed.
         cy.getBySel("home-pro-cta-register").should("exist");
 
@@ -87,69 +204,5 @@ describe('Pro', () => {
 
         // Trial CTA is there.
         cy.getBySel("home-pro-cta-trial").should("exist");
-    });
-
-    // Free user clicks on home CTA already-member link
-    it('free user clicks on home CTA already a member', () => {
-        // Registration CTA is displayed.
-        cy.getBySel("home-pro-cta-register").should("exist");
-
-        // Click on the button in the CTA.
-        cy.getBySel('home-pro-cta-already-member').click();
-
-        // Free user logins.
-        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
-
-        // Trial CTA is there.
-        cy.getBySel("home-pro-cta-trial").should("exist");
-    });
-
-    // Free user clicks on favorite icon
-    it("free user adds favorites", () => {
-        // Go to lillatro page
-        cy.searchAndGoToSpotPage('lillat');
-
-        // Click on favorite button.
-        cy.getBySel('favorite-add-button').click();
-
-        // Free user logins.
-        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
-
-        // Email modal is displayed.
-        cy.getBySel('trial-modal').should('be.visible');
-
-        // Click on Trial start button.
-        cy.getBySel('trial-modal-cta').click();
-
-        // Subscription confirmation page displayed.
-        cy.getBySel("subscription-confirmed").should("exist");
-    });
-
-    // Free user clicks on calendar CTA
-    it("free user clicks on calendar CTA", () => {
-        // Go to lillatro page
-        cy.searchAndGoToSpotPage('lillat');
-
-        // Click on good times tab.
-        cy.getBySel('surf-spot-tab-good-times').click();
-
-        // Click on favorite button.
-        cy.getBySel('surf-spot-forecast-calendar-banner').click();
-
-        // Free user logins.
-        cy.login(freeEmail, freeName, Cypress.env('demo_users_password'));
-
-        // Email modal is displayed.
-        cy.getBySel('trial-modal').should('be.visible');
-
-        // Click on Trial start button.
-        cy.getBySel('trial-modal-cta').click();
-
-        // The calendar modal shows up.
-        // Close the modal.
-        cy.getBySel('modal-close').click();
-
-        // Subscription confirmation page displayed.
-        cy.getBySel("subscription-confirmed").should("exist");
-    });
+    }); */
 });
