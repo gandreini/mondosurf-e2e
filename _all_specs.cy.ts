83e1548232f6503a08092073b194ed05b9f89d99
Giulio Andreini <g.andreini@gmail.com>
2026-01-21 07:56:58 +0100

    Different specs import for web and mobile.

--- _all_specs.cy.ts
+++
@@ -1,8 +0,0 @@
-import './auth.cy';
-import './basic-navigation.cy';
-import './favorites.cy';
-import './comments.cy';
-// import './pro.cy';
-import './home.cy';
-import './calendar.cy';
-import './map.cy';
